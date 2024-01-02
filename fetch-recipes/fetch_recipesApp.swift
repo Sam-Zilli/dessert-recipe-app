91dd43a457e70fdc03c3780459978d2ac1aa77c2
Sam Zilli <samzilli01@gmail.com>
2024-01-02 17:39:22 -0500

    Initial Commit

---
+++ fetch-recipes/fetch_recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_recipesApp.swift
+//  fetch-recipes
+//
+//  Created by Sam on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct fetch_recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
